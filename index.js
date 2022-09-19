cd70596509341fd3e0017675afd5a18a20c6ee06
Jessica Turner <jmcclung3509@gmail.com>
2022-09-18 22:22:38 -0700

    Created function to get values and operator

--- index.js
+++ index.js
@@ -17,91 +17,67 @@ const equalBtn = document.getElementById("equals")
 const plusMinusBtn = document.getElementById("negative")
 const decimalBtn = document.getElementById("decimal")
 
-const nineBtn = document.getElementById("9").addEventListener("click", getValue)
-const eightBtn = document.getElementById("8").addEventListener("click", getValue)
-const sevenBtn = document.getElementById("7").addEventListener("click", getValue)
-const sixBtn = document.getElementById("6").addEventListener("click", getValue)
-const fiveBtn = document.getElementById("5").addEventListener("click", getValue)
-const fourBtn = document.getElementById("4").addEventListener("click", getValue)
-const threeBtn =  document.getElementById("3").addEventListener("click", getValue)
-const twoBtn = document.getElementById("2").addEventListener("click", getValue)
-const oneBtn = document.getElementById("1").addEventListener("click", getValue)
-const zeroBtn = document.getElementById("0").addEventListener("click", getValue)
+const nineBtn = document.getElementById("9")
+const eightBtn = document.getElementById("8")
+const sevenBtn = document.getElementById("7")
+const sixBtn = document.getElementById("6")
+const fiveBtn = document.getElementById("5")
+const threeBtn =  document.getElementById("3")
+const twoBtn = document.getElementById("2")
+const oneBtn = document.getElementById("1")
+const zeroBtn = document.getElementById("0")
 
 const operators = document.querySelectorAll(".operator_btn")
 for (let operator of operators){
     operator.addEventListener("click", getOperator)
-    operator.addEventListener("click", setNumState)
+   
 }
+
+const numbers = document.querySelectorAll(".number_btn")
+for(let number of numbers){
+    number.addEventListener("click", getValue )
+}
+
+
 equals.addEventListener("click", endEquation)
 
+operatorClicked = false
+equalClicked = false
 
-let numberComplete = false
-let isNum1 = false
-let isNum2 = false
-
-let num1 = ""
-let num2=""
-let operator = ""
+let num1 =""
+let num2= ""
+let operator 
+let operator1
+let operator2
 let inputText = ""
 let equationText = ""
 
+function clearInput() {
+    inputContainer.textContent = ""
+  inputText = ""
+}
 
 function getValue(e){
-   let value = e.target.textContent
+    value = e.target.textContent
+
 setInput(value)
 setEquation(value)
-}
 
-function getNum1(){
-    if (isNum1== true){
-        num1 = inputContainer.textContent
-    }
-return num1
-}
-function clearInput() {
-       inputContainer.textContent = ""
-     inputText = ""
-}
-
-function getNum2(){
-    if ( isNum2 == true){
-    
-    num2 = inputContainer.textContent  
-    console.log(num2)
-    }
-
-    return num2
 
 }
 
 
-
-function setNumState(){
-    if (isNum1 == false && isNum2 == false){
-
-        isNum1 = true
-        isNum2 = false
-} else if (isNum1 == true && isNum2 == false){
-   isNum1 = false
-    isNum2 = true
-}
-console.log(num1Done, num2Done)
-}
-
-function setNumState(){
-    num1Done = true
-    num2Done = true
-    console.log(num1Done, num2Done)
-    getNum2()
-
-}
 
 function setInput(value){
+
+
+
     inputText+=value
     inputContainer.textContent = inputText
+    
 
 }
+
 function setEquation(value){
     equationText += value
     equationContainer.textContent = equationText
@@ -109,53 +85,61 @@ function setEquation(value){
 
 function getOperator(e){
 
-    operator = e.target.textContent
-   setOperatorContainer(operator)
-    clearInput()
+operator = e.target.textContent
+setCurrentNum(operator)
+console.log(operator)
+
+        clearInput()
 
 }
+function setCurrentNum(operator) {
+ operator = operator
+        currentNum = inputContainer.textContent
+        if (num1 == ""){
+            num1 = currentNum
+           
+            operator1 = operator
+        } else {
+            num1 = num1
+      operator1 = operator1
+            num2 = currentNum
+           
+            operator2 = operator
+            console.log(num1, num2, operator1, operator2)
+     
+        
+        }
 
-function setOperatorContainer(operatorValue){
- operatorContainer.textContent = operatorValue
+        return {num1, num2, operator1}
+        console.log(num1, operator1, num2, operator2)
+        // return{ num1, num2, operator1}
+       
+
+  
 }
-function getOperatorValue(){
-    operator = operatorContainer.textContent
-    return operator
+function setOperator(operatorValue){
+operator = operatorValue
+
+
 }
+// function waitForData(){
+//     if (num2 !== ""){
+//         endEquation()
+//     }
+// }
 
+function endEquation(num1, num2, operator1){
 
-
-function endEquation(){
-    num1 = getNum1()
-    console.log(num1)
-operator = getOperatorValue()
-
-    num2= getNum2()
-    console.log(num2)
-    console.log(`num1 = ${num1}, operator=${operator}, num2= ${num2}`)
+let data = setCurrentNum()
+console.log(data)
 }
-
-
-
-
 
 function operate(num1, num2, operator){
+
     console.log(num1, num2, operator)
-    // if (e.target.textContent == "+"){
-   
-    //     return operator
-    //   }else if (e.target.textContent == "-"){
-    //     let  operator = "minus"
-    //     return operator
-    //   } else if (e.target.textContent == "x"){
-    //     let operator = "multiply"
-    //     return operator
-    //   } else {
-    //     let  operator = "divide"
-    //     return operator
-    //   }
 }
- 
+
+
 
 
 function add (num1, num2){
